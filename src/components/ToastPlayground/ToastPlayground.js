95dcb1f8873c16cd2114454249d95e03b3682f25
Daniel Stopka <stopka.d@gmail.com>
2023-10-10 18:59:49 +0200

    feat: exercise 2

--- src/components/ToastPlayground/ToastPlayground.js
+++ src/components/ToastPlayground/ToastPlayground.js
@@ -3,12 +3,14 @@ import React from "react";
 import Button from "../Button";
 
 import styles from "./ToastPlayground.module.css";
+import Toast from "../Toast";
 
 const VARIANT_OPTIONS = ["notice", "warning", "success", "error"];
 
 function ToastPlayground() {
   const [message, setMessage] = React.useState("");
   const [variant, setVariant] = React.useState(VARIANT_OPTIONS[0]);
+  const [isDisplayed, setIsDisplayed] = React.useState(false);
 
   return (
     <div className={styles.wrapper}>
@@ -16,6 +18,15 @@ function ToastPlayground() {
         <img alt="Cute toast mascot" src="/toast.png" />
         <h1>Toast Playground</h1>
       </header>
+
+      {/* notice | warning | success | error */}
+      <Toast
+        isDisplayed={isDisplayed}
+        variant={variant}
+        handleClose={() => setIsDisplayed(false)}
+      >
+        {message}
+      </Toast>
 
       <div className={styles.controlsWrapper}>
         <div className={styles.row}>
@@ -58,7 +69,9 @@ function ToastPlayground() {
         <div className={styles.row}>
           <div className={styles.label} />
           <div className={`${styles.inputWrapper} ${styles.radioWrapper}`}>
-            <Button>Pop Toast!</Button>
+            <Button onClick={() => setIsDisplayed(!isDisplayed)}>
+              Pop Toast!
+            </Button>
           </div>
         </div>
       </div>
